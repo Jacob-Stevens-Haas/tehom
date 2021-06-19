fedaadca9d41cb177dbeafcd7bee901cb6b35696
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-19 11:23:44 -0700

    Rejig setup.cfg, setup.py, and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(name='choshek',
-      version='0.0.0',
-      description='Machine Learning on underwater acoustics',
-      url='https://github.com/Jake-Stevens-Haas/choshek',
-      author='Jake Stevens-Haas',
-      author_email='jacob.stevens.haas@gmail.com',
-      license='AGPL',
-      packages=[''], # Packages & subpackages for the directory
-                              # containintby setup.py
-      py_modules=[''], #This package's modules
-      install_requires=[],
-      zip_safe=False,
-      python_requires='~=3.6')
+setup()
