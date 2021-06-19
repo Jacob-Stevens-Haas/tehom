2db8ff28635d810aed145382214e4cdf7e3fba78
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-19 11:32:58 -0700

    versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup()
+import versioneer
+
+
+
+setup(
+      cmdclass = versioneer.get_cmdclass(),
+      version = versioneer.get_version()
+)
