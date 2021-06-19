8d3134c75fee8d228085e1cfa5949da1f0dd257a
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-19 16:37:27 -0700

    CLN: Black the code

--- tehom/datalib.py
+++ tehom/datalib.py
@@ -11,30 +11,29 @@ With the datalib, you can:
 * Show the data either available for download or already downloaded
 * Sample downloaded data into a labeled format, ready for ``model.fit``
 """
-#Standard Library
-from pathlib import Path
 from dataclasses import dataclass
 from datetime import datetime
+# Standard Library
+from pathlib import Path
+# Types for Type Hints
+from typing import List, Tuple, Union
 
-#3rd Party
+# 3rd Party
 import pandas as pd
-
-#Types for Type Hints
-from typing import Union, List, Tuple
 from matplotlib.figure import Figure as MFigure
-from plotly.graph_objs._figure import Figure as PFigure
 from pandas._libs.tslibs.timedeltas import Timedelta
 from pandas._libs.tslibs.timestamps import Timestamp
 from pandas.core.frame import DataFrame
+from plotly.graph_objs._figure import Figure as PFigure
 
-#Local modules
+# Local modules
 from . import shared
 
-ais_site = 'https://coast.noaa.gov/htdata/CMSP/AISDataHandler/'
-ais_db = shared.storage / 'ais.db'
-onc_db = shared.storage / 'onc.db'
+ais_site = "https://coast.noaa.gov/htdata/CMSP/AISDataHandler/"
+ais_db = shared.storage / "ais.db"
+onc_db = shared.storage / "onc.db"
 
-# %% AIS downloads
+
 def download_ships(year: int, month: int, zone: int) -> None:
     """Download AIS records from Marine Cadastre.  Records are stored in
     a local sqlite database.  Marine Cadastre organizes records into
@@ -51,16 +50,16 @@ def download_ships(year: int, month: int, zone: int) -> None:
 
 
 def init_ais_db(ais_db: Union[Path, str]) -> None:
-    """Initializes the local AIS record database, if it does not exist
-    """
+    """Initializes the local AIS record database, if it does not exist"""
     raise NotImplementedError
 
-# %% ONC Downloads
 
-def download_acoustics(hydrophones: List[str],
-                    begin: Union[datetime, str, Timestamp],
-                    end: Union[datetime, str, Timestamp],
-                    extension: str) -> None:
+def download_acoustics(
+    hydrophones: List[str],
+    begin: Union[datetime, str, Timestamp],
+    end: Union[datetime, str, Timestamp],
+    extension: str,
+) -> None:
     """Download acoustic data from ONC.  Data is stored in files in the
     package data folder, but metadata and paths are stored in a local
     sqlite database.
@@ -80,19 +79,21 @@ def download_acoustics(hydrophones: List[str],
     init_onc_db(onc_db)
     raise NotImplementedError
 
+
 def init_onc_db(onc_db: Union[Path, str]) -> None:
     """Initializes the local acoustic file database, if it does not
     exist.
     """
     raise NotImplementedError
 
-# %% Visualization of available data
 
-def show_available_data(begin: Union[datetime, str, Timestamp],
-                        end: Union[datetime, str, Timestamp],
-                        bottomleft: Tuple[float]=(-90., -180.),
-                        topright: Tuple[float]=(90., 180.),
-                        style: str='map') -> Union[MFigure, PFigure]:
+def show_available_data(
+    begin: Union[datetime, str, Timestamp],
+    end: Union[datetime, str, Timestamp],
+    bottomleft: Tuple[float] = (-90.0, -180.0),
+    topright: Tuple[float] = (90.0, 180.0),
+    style: str = "map",
+) -> Union[MFigure, PFigure]:
     """Creates a visualization of what data is available to download and
     what data is available locally for sampling.
 
@@ -113,27 +114,31 @@ def show_available_data(begin: Union[datetime, str, Timestamp],
     """
     raise NotImplementedError
 
-# %% The Big Kahuna - sampling data
+
 @dataclass
 class SampleParams:
     """Settings for repeatable data samples.  For you probability nerds,
     this should control the X in X(omega), while the other parameters to
     sample() control the omega.
     """
-    interval: Union[str, Timedelta] = '5 min'
-    duration: Union[str, Timedelta] = '1 second'
-    extension: str = 'mp3'
+
+    interval: Union[str, Timedelta] = "5 min"
+    duration: Union[str, Timedelta] = "1 second"
+    extension: str = "mp3"
 
     interval = pd.Timedelta(interval)
     duration = pd.Timedelta(duration)
-    if extension not in ['mp3']:
-        raise ValueError('Only extension with sample() implemented is mp3')
+    if extension not in ["mp3"]:
+        raise ValueError("Only extension with sample() implemented is mp3")
 
-def sample(hydrophones: List[str],
-            begin: Union[datetime, str, Timestamp],
-            end: Union[datetime, str, Timestamp],
-            sample_params: SampleParams,
-            verbose: bool=False) -> DataFrame:
+
+def sample(
+    hydrophones: List[str],
+    begin: Union[datetime, str, Timestamp],
+    end: Union[datetime, str, Timestamp],
+    sample_params: SampleParams,
+    verbose: bool = False,
+) -> DataFrame:
     """Sample the downloaded acoustics and AIS data to create a labeled
     dataset for machine learning, ready for ``model.fit()``.
 
