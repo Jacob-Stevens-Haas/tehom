8b131cf03bcd47cfbe555afbd1e4011d583a35bd
mwmasters <mwmasters10@gmail.com>
2021-08-09 11:32:27 -0700

    wrote test_download_ais_to_temp

---
+++ tehom/tests/test_downloads.py
@@ -0,0 +1,10 @@
+from tehom import _downloads, _persistence
+
+
+def test_download_ais_to_temp(declare_stateful):
+    year = 2014
+    month = 1
+    zone = 1
+    _downloads._download_ais_to_temp(year, month, zone)
+    path = _persistence.AIS_TEMP_DIR / f"{year}_{month}_{zone}.zip"
+    assert path.exists()
